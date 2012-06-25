47f1b7efd3ec82162234dc95c60bf3c79de18412
Roman Redkovich <redkovich@gmail.com>
2012-06-25 17:16:15 +0400

    begun to write main

--- main.c
+++ main.c
@@ -1,1 +1,23 @@
-aaa
+#include <msp430.h>
+#include "winstar-display.h"
+
+
+int main () {
+	
+	WDTCTL = WDTPW + WDTHOLD; 	//Stop watchdog timer
+	BCSCTL1 = CALBC1_8MHZ;		//Frequency have been set
+	DCOCTL = CALDCO_8MHZ;		//Frequency have been set
+	
+	struct dataBus db;
+	struct readWrite rw;
+	
+	
+	
+	ow.port_out = &P1OUT;
+	ow.port_in = &P1IN;
+	ow.port_ren = &P1REN;
+	ow.port_dir = &P1DIR;
+	ow.pin = BIT4;
+	
+	
+};
