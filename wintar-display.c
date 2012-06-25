f2d2e041be081d4a619547f0a08aa459b07a8bc9
Roman Redkovich <redkovich@gmail.com>
2012-06-25 16:34:03 +0400

    function set
    initialization in process

--- wintar-display.c
+++ wintar-display.c
@@ -7,19 +7,17 @@ void initialize (dataBus *db, readWrite *rw){
 		//function set
 			*(rw->port) &= ~rw->RS; 	// эта хрень делает на ноге RS нуль
 			*(rw->port) &= ~rw->RW;
-			*( &= ~DB7;
-			P1OUT &= ~DB6;
-			P1OUT |= DB5;	//а эта хрень делает на ноге DB5 единицу
-			P1OUT |= DB4;
-			P1OUT |= DB3;
-			P2OUT |= DB2;
-			P2OUT |= DB1; 	//russian font
-			P1OUT &= ~DB0;	//russian font
+			*(db->port) &= ~db->DB7;
+			*(db->port) &= ~db->DB6;
+			*(db->port) |= db->DB5;	//а эта хрень делает на ноге DB5 единицу
+			*(db->port) |= db->DB4;
+			*(db->port) |= db->DB3;
+			*(db->port) |= db->DB2;
+			*(db->port) |= db->DB1; 	//russian font
+			*(db->port) &= ~DB0;	//russian font
 			
 			//read command
-			P2OUT  |= E;
-			DELAY_MS(250);
-			P2OUT &= ~E;
+			void run ();
 	
 	//DELAY_MS (10);			//задержка в мс
 	
